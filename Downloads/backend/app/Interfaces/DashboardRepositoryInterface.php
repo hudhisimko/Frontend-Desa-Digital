5d5fee95b6d145604f628d452136275a9ccba87b
znfdhlaa <dhilasmkthp@gmail.com>
2025-08-15 09:44:40 +0700

    Delete Downloads/backend directory

--- Downloads/backend/app/Interfaces/DashboardRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Interfaces;
-
-interface DashboardRepositoryInterface
-{
-    public function getDashboardData();
-}
--- Downloads/backend/bootstrap/providers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\RepositoryServiceProvider::class,
-    Spatie\Permission\PermissionServiceProvider::class,
-];
