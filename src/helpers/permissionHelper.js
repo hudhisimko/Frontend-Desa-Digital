f8af7798960748c136bbb3da9e2825d81954ed49
Hudhi Simko <134993164+hudhisimko@users.noreply.github.com>
2025-08-20 21:41:25 +0700

    Delete src directory

--- src/helpers/permissionHelper.js
+++
@@ -1,9 +0,0 @@
-import {useAuthStore} from '@/stores/auth'
-
-export const can = permissions => {
-    const authStore = useAuthStore()
-
-    const userPermissions = authStore.user?.permissions || []
-
-    return userPermissions.includes(permissions)
-}
